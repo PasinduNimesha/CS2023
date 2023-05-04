3335228ef559f67cf7474aee29365a82e3284fa2
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2023-05-04 16:51:16 +0530

    Update heap.cpp

--- week8/heap.cpp
+++ week8/heap.cpp
@@ -1,66 +1,80 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+ using namespace std; 
+    
+ // function to heapify the tree 
+ void heapify(int arr[], int n, int root) 
+ { 
+     int largest = root; 
+     int left = 2*root + 1; 
+     int right = 2*root + 2; 
   
-// function to heapify the tree
-void heapify(int arr[], int n, int root)
-{
-    int largest = root;
-    int left = 2*root + 1;
-    int right = 2*root + 2;
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != root)
-    {
-        swap(arr[root], arr[largest]);
-
-        heapify(arr, n, largest);
+     if (left < n && arr[left] > arr[largest]) 
+         largest = left; 
+  
+     if (right < n && arr[right] > arr[largest]) 
+         largest = right; 
+  
+     if (largest != root) 
+     { 
+         swap(arr[root], arr[largest]); 
+  
+         heapify(arr, n, largest); 
+     } 
+ } 
+    
+ // implementing heap sort 
+ void heapSort(int arr[], int n) 
+ { 
+     for (int i = n / 2 - 1; i >= 0; i--) 
+         heapify(arr, n, i); 
+  
+     for (int i=n-1; i>=0; i--) 
+     { 
+         swap(arr[0], arr[i]); 
+  
+         heapify(arr, i, 0); 
+     } 
+ } 
+    
+ /* print contents of array */ 
+ void displayArray(int arr[], int n) 
+ { 
+    for (int i=0; i<n; ++i) 
+    cout << arr[i] << " "; 
+    cout << "\n"; 
+ } 
+    
+ // main program 
+ int main() 
+ { 
+    cout<<"Manuel array input(0) or random array(1): "<<endl;
+    int choice;
+    cin >> choice;
+    cout<<"Enter the array size: "<<endl; 
+    int n; 
+    cin >> n; 
+    int heap_arr[n]; 
+    if(choice == 0){
+    cout << "Enter "<<n<<" numbers separated by spaces: "; 
+  
+    for(int i = 0; i < n; i++) { 
+       cin >> heap_arr[i]; 
+    } 
     }
-}
-  
-// implementing heap sort
-void heapSort(int arr[], int n)
-{
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (int i=n-1; i>=0; i--)
-    {
-        swap(arr[0], arr[i]);
-
-        heapify(arr, i, 0);
+    else if (choice == 1){
+    	srand(time(NULL));
+    	for (int i = 0; i < n; i++) {
+        heap_arr[i] = rand()%1000;
     }
-}
-  
-/* print contents of array */
-void displayArray(int arr[], int n)
-{
-   for (int i=0; i<n; ++i)
-   cout << arr[i] << " ";
-   cout << "\n";
-}
-  
-// main program
-int main()
-{
-   cout<<"Enter the array size: "<<endl;
-   int n;
-   cin >> n;
-   int heap_arr[n];
-   cout << "Enter "<<n<<" numbers separated by spaces: ";
-
-   for(int i = 0; i < n; i++) {
-      cin >> heap_arr[i];
-   }
-   cout<<"Input array"<<endl;
-   displayArray(heap_arr,n);
-  
-   heapSort(heap_arr, n);
-  
-   cout << "Sorted array"<<endl;
-   displayArray(heap_arr, n);
-}
+    	
+    }
+    cout<<"Input array"<<endl; 
+    displayArray(heap_arr,n); 
+    
+    heapSort(heap_arr, n); 
+    
+    cout << "Sorted array"<<endl; 
+    displayArray(heap_arr, n); 
+ }
